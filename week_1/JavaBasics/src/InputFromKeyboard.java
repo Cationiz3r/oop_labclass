2b1c6254e5c7eaa85141a1d10de1522121c5bfc0
Martin Cation <martincation@protonmail.com>
2023-04-25 15:04:01 +0700

    w1: java keyboard io

---
+++ week_1/JavaBasics/src/InputFromKeyboard.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class InputFromKeyboard {
+	public static void main(String[] args) {
+		var scanner = new Scanner(System.in);
+
+		System.out.print("What's your name? ");
+		var strName = scanner.nextLine();
+		System.out.print("How old are you? ");
+		var iAge = scanner.nextInt();
+		System.out.print("How tall are you (m)? ");
+		var dHeight = scanner.nextDouble();
+
+		System.out.println(
+			"Mrs/Ms. " + strName + ", " +
+			iAge + " year old. " +
+			"Your height is " + dHeight + "m."
+		);
+
+		scanner.close();
+	}
+}
