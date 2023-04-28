30b558f126169b8786418550f9a63e63c60bad65
Martin Cation <martincation@protonmail.com>
2023-04-28 13:40:20 +0700

    w2: main func for aims project

---
+++ week_2/AimsProject/Aims.java
@@ -0,0 +1,31 @@
+public class Aims {
+	public static void main(String[] args) {
+		var anOrder = new Cart();
+
+		var dvd1 = new DigitalVideoDisc(
+			"The Lion King",
+			"Animation",
+			"Roger Allers",
+			87,
+			19.95f
+		);
+		var dvd2 = new DigitalVideoDisc(
+				"Star Wars",
+				"Science Fiction",
+				"George Lucas",
+				87,
+				24.95f
+		);
+		var dvd3 = new DigitalVideoDisc(
+				"Aladin",
+				"Animation",
+				18.99f
+		);
+
+		anOrder.addDigitalVideoDisc(dvd1);
+		anOrder.addDigitalVideoDisc(dvd2);
+		anOrder.addDigitalVideoDisc(dvd3);
+
+		System.out.println("Total Cost is: " + anOrder.totalCost());
+	}
+}
