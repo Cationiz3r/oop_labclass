e560518e1896c0ecac9f8813ab408f6a37ba4e7d
Martin Cation <martincation@protonmail.com>
2023-05-01 16:37:43 +0700

    project: extra functions for compact disc
    its length is now the total length of its tracks,
    track adder/remover functions

--- AimsProject/src/main/java/hust/soict/dsai/aims/media/CompactDisc.java
+++ AimsProject/src/main/java/hust/soict/dsai/aims/media/CompactDisc.java
@@ -9,4 +9,22 @@ extends Disc {
 
 	public String getArtist() { return artist; }
 	public ArrayList<Track> getTracks() { return tracks; }
+
+	@Override
+	public int getLength() {
+		return tracks.stream().mapToInt(t -> t.getLength()).sum();
+	}
+
+	public void addTrack(Track track) {
+		if (tracks.contains(track)) {
+			return;
+		}
+		tracks.add(track);
+	}
+	public void removeTrack(Track track) {
+		if (!tracks.contains(track)) {
+			return;
+		}
+		tracks.remove(track);
+	}
 }
