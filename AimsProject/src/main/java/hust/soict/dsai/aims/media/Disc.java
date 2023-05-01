43acbc3f09de5e59df51bc13c39e8ce7b4239a78
Martin Cation <martincation@protonmail.com>
2023-05-01 16:18:50 +0700

    project: base class with extra fields

---
+++ AimsProject/src/main/java/hust/soict/dsai/aims/media/Disc.java
@@ -0,0 +1,10 @@
+package hust.soict.dsai.aims.media;
+
+public class Disc
+extends Media {
+	protected String director;
+	protected int length;
+
+	public String getDirector() { return director; }
+	public int getLength() { return length; }
+}
