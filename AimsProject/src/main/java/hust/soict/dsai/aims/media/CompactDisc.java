1d912ec8a095f095e747adae97f034222137ea47
Martin Cation <martincation@protonmail.com>
2023-05-01 16:27:40 +0700

    project: employ list of tracks for compact disc

--- AimsProject/src/main/java/hust/soict/dsai/aims/media/CompactDisc.java
+++ AimsProject/src/main/java/hust/soict/dsai/aims/media/CompactDisc.java
@@ -1,6 +1,12 @@
 package hust.soict.dsai.aims.media;
 
+import java.util.ArrayList;
+
 public class CompactDisc
 extends Disc {
+	private String artist;
+	private ArrayList<Track> tracks = new ArrayList<>();
 
+	public String getArtist() { return artist; }
+	public ArrayList<Track> getTracks() { return tracks; }
 }
