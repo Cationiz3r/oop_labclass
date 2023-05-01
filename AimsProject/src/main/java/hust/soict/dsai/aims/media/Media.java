5a5f54360d48c62615639f81133d2d7440771a81
Martin Cation <martincation@protonmail.com>
2023-05-01 19:32:01 +0700

    project/media: assign id with static field of base

--- AimsProject/src/main/java/hust/soict/dsai/aims/media/Media.java
+++ AimsProject/src/main/java/hust/soict/dsai/aims/media/Media.java
@@ -1,10 +1,19 @@
 package hust.soict.dsai.aims.media;
 
 public abstract class Media {
+	private static int nItems = 0;
+
 	protected int id;
 	protected String title;
 	protected String category;
 	protected float cost;
+
+	public Media(String title, String category, float cost) {
+		id = nItems++;
+		this.title = title;
+		this.category = category;
+		this.cost = cost;
+	}
 
 	public int getId() { return id; }
 	public String getTitle() { return title; }
