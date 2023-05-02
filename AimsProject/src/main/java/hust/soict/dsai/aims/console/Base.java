6fb8bb97682e3922d51d988e7e92340012712105
Martin Cation <martincation@protonmail.com>
2023-05-02 10:48:24 +0700

    tui/base: schema for tui menu

---
+++ AimsProject/src/main/java/hust/soict/dsai/aims/console/Base.java
@@ -0,0 +1,39 @@
+package hust.soict.dsai.aims.console;
+
+import java.util.List;
+import java.util.Scanner;
+import hust.soict.dsai.aims.cart.Cart;
+import hust.soict.dsai.aims.store.Store;
+
+public abstract class Base {
+	private boolean inLoop = true;
+	protected Store store;
+	protected Cart cart;
+	protected Scanner scanner;
+	protected List<String> options;
+
+	Base(Store store, Cart cart, Scanner scanner) {
+		this.store = store;
+		this.cart = cart;
+	}
+
+	protected abstract void displayInfo();
+	protected abstract void handleChoice(int choice);
+
+	public void display() {
+		while (inLoop) {
+			System.out.print("\033[H\033[2J"); // Clear screen
+			System.out.flush();
+
+			displayInfo();
+			System.out.println("Options:\n---------------------------------");
+			for (int i = 0; i < options.size(); ++i)
+				System.out.println(i + 1 + ". " + options.get(i));
+			System.out.println("0. Back");
+			System.out.println("---------------------------------");
+			System.out.print("Please choose a number: ");
+
+			handleChoice(scanner.nextInt());
+		}
+	}
+}
