ade4d1343f757975152e9478ab1746b280be3936
Martin Cation <martincation@protonmail.com>
2023-05-04 15:31:20 +0700

    project/media: one constuctor requires all fields

--- AimsProject/src/main/java/hust/soict/dsai/aims/media/DigitalVideoDisc.java
+++ AimsProject/src/main/java/hust/soict/dsai/aims/media/DigitalVideoDisc.java
@@ -2,31 +2,8 @@ package hust.soict.dsai.aims.media;
 
 public class DigitalVideoDisc
 extends Disc {
-	private static int nbDigitalVideoDiscs = 0;
-
-	public DigitalVideoDisc(String title) {
-		this.title = title;
-		this.id = nbDigitalVideoDiscs;
-		nbDigitalVideoDiscs++;
-	}
-	public DigitalVideoDisc(String title, String category, float cost) {
-		this(title);
-		this.category = category;
-		this.cost = cost;
-	}
-	public DigitalVideoDisc(String title, String category, String director, float cost) {
-		this(title, category, cost);
-		this.director = director;
-	}
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-		this(title, category, director, cost);
-		this.length = length;
-	}
-
-	public void setTitle(String title) { this.title = title; }
-
-	public boolean isMatch(String title) {
-		return this.title == title;
+		super(title, category, director, length, cost);
 	}
 
 	public String toString() {
