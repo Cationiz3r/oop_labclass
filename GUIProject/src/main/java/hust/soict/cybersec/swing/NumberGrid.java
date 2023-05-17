3165318579b95280c9c6cc9ec659d06c7fbc25eb
Martin Cation <martincation@protonmail.com>
2023-05-18 00:00:13 +0700

    gui/number_grid: populate center pabel with buttons

--- GUIProject/src/main/java/hust/soict/cybersec/swing/NumberGrid.java
+++ GUIProject/src/main/java/hust/soict/cybersec/swing/NumberGrid.java
@@ -33,12 +33,32 @@ extends JFrame {
 		cp.add(tfDisplay, BorderLayout.NORTH);
 
 		JPanel panelButtons = new JPanel(new GridLayout(4, 3));
-		// addButtons(panelButtons);
+		addButtons(panelButtons);
 		cp.add(panelButtons, BorderLayout.CENTER);
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Number grid");
 		setSize(200, 200);
 		setVisible(true);
+	}
+
+	private void addButtons(JPanel panelButtons) {
+		var listener = new ButtonListener();
+		for (int i = 0; i <= 9; ++i) {
+			btnNumbers[i] = new JButton("" + i);
+			btnNumbers[i].addActionListener(listener);
+			if (i == 0) continue;
+			panelButtons.add(btnNumbers[i]);
+		}
+
+		btnDelete = new JButton("DEL");
+		btnDelete.addActionListener(listener);
+		panelButtons.add(btnDelete);
+
+		panelButtons.add(btnNumbers[0]);
+
+		btnReset = new JButton("C");
+		btnReset.addActionListener(listener);
+		panelButtons.add(btnReset);
 	}
 }
