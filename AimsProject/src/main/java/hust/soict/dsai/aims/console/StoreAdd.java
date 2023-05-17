e63fcad0b78a985ba68cd75abd5c3a752ad2aeb2
Martin Cation <martincation@protonmail.com>
2023-05-17 16:53:59 +0700

    tui/store_add: user inputs

--- AimsProject/src/main/java/hust/soict/dsai/aims/console/StoreAdd.java
+++ AimsProject/src/main/java/hust/soict/dsai/aims/console/StoreAdd.java
@@ -20,6 +20,42 @@ extends Base {
 
 	@Override
 	protected void handleChoice(int choice) {
+		System.out.print("Enter title: ");
+		var title = scanner.nextLine().trim();
+
+		if (title.isEmpty()) {
+			System.out.println("An item can not have an empty title!");
+			scanner.nextLine();
+			return;
+		}
+		if (store.searchByTitle(title) != null) {
+			System.out.println("An item with title already exists!");
+			scanner.nextLine();
+			return;
+		}
+
+		System.out.print("Enter category: ");
+		var category = scanner.nextLine().trim();
+
+		String director = null;
+		int length = 0;
+		if (choice > 1) {
+			System.out.print("Enter director: ");
+			director = scanner.nextLine().trim();
+
+			System.out.print("Enter duration: ");
+			try {
+				length = scanner.nextInt();
+			} catch (InputMismatchException e) {}
+			scanner.nextLine();
+		}
+
+		float cost = 0;
+		System.out.print("Enter cost: ");
+		try {
+			cost = scanner.nextFloat();
+		} catch (InputMismatchException e) {}
+		scanner.nextLine();
 
 	}
 }
