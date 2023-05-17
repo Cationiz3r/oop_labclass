b464ebcd61c526fa9053fbf6f5398752a8f7fd42
Martin Cation <martincation@protonmail.com>
2023-05-17 14:45:50 +0700

    tui/store_manage: menu for adding & remove action

---
+++ AimsProject/src/main/java/hust/soict/dsai/aims/console/StoreAdd.java
@@ -0,0 +1,25 @@
+package hust.soict.dsai.aims.console;
+
+import java.util.Arrays;
+
+public class StoreAdd
+extends Base {
+	public StoreAdd(Base menu) {
+		super(menu);
+		options = Arrays.asList(
+			"Book",
+			"Compact disc",
+			"DVD"
+		);
+	}
+
+	@Override
+	protected void displayInfo() {
+		System.out.println("Add a media:\n");
+	}
+
+	@Override
+	protected void handleChoice(int choice) {
+
+	}
+}
--- AimsProject/src/main/java/hust/soict/dsai/aims/console/StoreManage.java
+++ AimsProject/src/main/java/hust/soict/dsai/aims/console/StoreManage.java
@@ -1,6 +1,7 @@
 package hust.soict.dsai.aims.console;
 
 import java.util.Arrays;
+import hust.soict.dsai.aims.media.Media;
 
 public class StoreManage
 extends Base {
@@ -20,6 +21,21 @@ extends Base {
 
 	@Override
 	protected void handleChoice(int choice) {
+		switch (choice) {
+		case 1:
+			new StoreAdd(this).display();
+			break;
+		case 2:
+			Media item = null;
+			var search = scanner.nextLine();
+			item = store.searchByTitle(search.trim());
 
+			if (item == null)
+				System.out.println("\nNo item with that title!");
+			else
+				System.out.println("Item has been removed!");
+
+			store.removeMedia(item);
+		}
 	}
 }
