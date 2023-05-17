34c8e2137302af9ff8e0781d2fe7d8824a1d764f
Martin Cation <martincation@protonmail.com>
2023-05-17 13:11:18 +0700

    tui/cart: direct search item options

--- AimsProject/src/main/java/hust/soict/dsai/aims/console/Cart.java
+++ AimsProject/src/main/java/hust/soict/dsai/aims/console/Cart.java
@@ -1,6 +1,7 @@
 package hust.soict.dsai.aims.console;
 
 import java.util.Arrays;
+import hust.soict.dsai.aims.media.Media;
 
 public class Cart
 extends Base {
@@ -23,6 +24,19 @@ extends Base {
 
 	@Override
 	protected void handleChoice(int choice) {
+		Media item = null;
+		if (choice >= 3 && choice <= 4) {
+			System.out.print("Enter title of item: ");
+			var search = scanner.nextLine();
+			item = store.searchByTitle(search.trim());
+
+			if (item == null) {
+				System.out.println("\nNo item with that title!");
+				scanner.nextLine();
+				return;
+			}
+		}
+
 		switch(choice) {
 		case 1:
 			new CartFilter(this).display();
@@ -30,5 +44,18 @@ extends Base {
 		case 2:
 			new CartSort(this).display();
 			break;
+		case 3:
+			cart.removeMedia(item);
+			scanner.nextLine();
+			break;
+		case 4:
+			item.tryPlay();
+			scanner.nextLine();
+			break;
+		case 5:
+			cart.placeOrder();
+			scanner.nextLine();
+			break;
+		}
 	}
 }
