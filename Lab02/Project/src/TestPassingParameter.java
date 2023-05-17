358688c23637eee2591c436b7fe1749277211412
Martin Cation <martincation@protonmail.com>
2023-05-17 10:22:51 +0700

    project/test: dvd wrapper swap

--- Lab02/Project/src/TestPassingParameter.java
+++ Lab02/Project/src/TestPassingParameter.java
@@ -1,3 +1,17 @@
+class DVDWrapper {
+	public DigitalVideoDisc dvd;
+
+	public DVDWrapper(DigitalVideoDisc dvd) {
+		this.dvd = dvd;
+	}
+
+	public void swap(DVDWrapper wrapper) {
+		DigitalVideoDisc tmp = dvd;
+		dvd = wrapper.dvd;
+		wrapper.dvd = tmp;
+	}
+}
+
 public class TestPassingParameter {
 	public static void swap(Object o1, Object o2) {
 		Object tmp = o1;
@@ -19,7 +33,15 @@ public class TestPassingParameter {
 		System.out.println("Jungle DVD title: " + jungleDVD.getTitle());
 		System.out.println("Cinderella DVD title: " + cinderellaDVD.getTitle());
 
-		changeTitle(jungleDVD, cinderellaDVD.getTitle());
-		System.out.println("Jungle DVD title: " + jungleDVD.getTitle());
+		// changeTitle(jungleDVD, cinderellaDVD.getTitle());
+		// System.out.println("Jungle DVD title: " + jungleDVD.getTitle());
+
+		System.out.println("Swapping DVDs using wrappers...");
+		var jungleWrapper = new DVDWrapper(jungleDVD);
+		var cinderellaWrapper = new DVDWrapper(cinderellaDVD);
+
+		jungleWrapper.swap(cinderellaWrapper);
+		System.out.println("Jungle DVD title: " + jungleWrapper.dvd.getTitle());
+		System.out.println("Cinderella DVD title: " + cinderellaWrapper.dvd.getTitle());
 	}
 }
