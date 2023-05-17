d5b80e8647b18a1f927650207b47bda0c16c276f
Martin Cation <martincation@protonmail.com>
2023-05-17 14:34:40 +0700

    tui/cart_filter: complete search

--- AimsProject/src/main/java/hust/soict/dsai/aims/console/CartFilter.java
+++ AimsProject/src/main/java/hust/soict/dsai/aims/console/CartFilter.java
@@ -1,6 +1,7 @@
 package hust.soict.dsai.aims.console;
 
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import hust.soict.dsai.aims.media.Media;
 
 public class CartFilter
@@ -25,9 +26,22 @@ extends Base {
 
 		switch(choice) {
 		case 1:
+			System.out.print("Enter item's ID: ");
+			try {
+				int id = scanner.nextInt();
+				scanner.nextLine(); // Consume newline char
+				item = cart.searchById(id);
+			} catch (InputMismatchException e) {}
 			break;
 		case 2:
+			System.out.print("Enter title of item: ");
+			var search = scanner.nextLine();
+			item = cart.searchByTitle(search.trim());
 			break;
 		}
+		if (item != null) System.out.println(item.toString());
+		else System.out.println("No item with that criteria");
+
+		scanner.nextLine();
 	}
 }
