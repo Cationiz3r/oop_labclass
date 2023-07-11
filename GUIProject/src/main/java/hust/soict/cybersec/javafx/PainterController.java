c19cab871a2b1764a6375a73612ba1ac021db548
Martin Cation <martincation@protonmail.com>
2023-07-11 14:13:35 +0700

    painter: change brush color based on active tool

--- GUIProject/src/main/java/hust/soict/cybersec/javafx/PainterController.java
+++ GUIProject/src/main/java/hust/soict/cybersec/javafx/PainterController.java
@@ -2,6 +2,7 @@ package hust.soict.cybersec.javafx;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
@@ -9,12 +10,22 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class PainterController {
+	private Color brushColor = Color.BLACK;
 	@FXML
 	private Pane drawingAreaPane;
 	@FXML private ToggleGroup tool;
 
 	@FXML
 	void changeTool(ActionEvent event) {
+		switch(((RadioButton)event.getSource()).getText()) {
+		default:
+		case "Pen":
+			brushColor = Color.BLACK;
+			break;
+		case "Eraser":
+			brushColor = Color.WHITE;
+			break;
+		}
 	}
 
 	@FXML
@@ -24,7 +35,7 @@ public class PainterController {
 
 	@FXML
 	void drawingAreaMouseDragged(MouseEvent event) {
-		var circle = new Circle(event.getX(), event.getY(), 4, Color.BLACK);
+		var circle = new Circle(event.getX(), event.getY(), 4, brushColor);
 		drawingAreaPane.getChildren().add(circle);
 	}
 }
