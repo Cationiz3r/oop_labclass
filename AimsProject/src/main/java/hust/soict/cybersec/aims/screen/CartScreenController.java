e18b72ec008cc743919ac27f71a1f69ca649f116
Martin Cation <martincation@protonmail.com>
2023-07-11 17:18:20 +0700

    screen/cart: play/remove button visibility based on item

--- AimsProject/src/main/java/hust/soict/cybersec/aims/screen/CartScreenController.java
+++ AimsProject/src/main/java/hust/soict/cybersec/aims/screen/CartScreenController.java
@@ -2,7 +2,10 @@ package hust.soict.cybersec.aims.screen;
 
 import hust.soict.cybersec.aims.cart.Cart;
 import hust.soict.cybersec.aims.media.Media;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.ToggleGroup;
@@ -16,6 +19,8 @@ public class CartScreenController {
 	@FXML private TableColumn<Media, String> colMediaTitle;
 	@FXML private TableColumn<Media, String> colMediaCategory;
 	@FXML private TableColumn<Media, Float> colMediaCost;
+	@FXML private Button btnPlay;
+	@FXML private Button btnRemove;
 
 	public CartScreenController(Cart cart) { this.cart = cart; }
 
@@ -25,5 +30,16 @@ public class CartScreenController {
 		colMediaCategory.setCellValueFactory(new PropertyValueFactory<Media, String>("category"));
 		colMediaCost.setCellValueFactory(new PropertyValueFactory<Media, Float>("cost"));
 		tblMedia.setItems(cart.getItemsOrdered());
+
+		btnPlay.setVisible(false);
+		btnRemove.setVisible(false);
+		tblMedia.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Media>() {
+			@Override
+			public void changed(ObservableValue<? extends Media> observable, Media oldValue, Media newValue) {
+				if (newValue == null) return;
+				btnRemove.setVisible(true);
+				btnPlay.setVisible(newValue.isPlayable());
+			}
+		});
 	}
 }
